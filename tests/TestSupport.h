598c0f9289ef69785850519bcc5496c8c5245fc6
wsy <shunyi_wang@163.com>
2026-07-26 12:29:55 +0800

    chore: trim public repository materials

--- tests/TestSupport.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <cstdlib>
-#include <iostream>
-#define CHECK(expr) do { if (!(expr)) { \
-    std::cerr << __FILE__ << ":" << __LINE__ << " CHECK failed: " #expr << std::endl; \
-    std::exit(1); \
-} } while (0)
